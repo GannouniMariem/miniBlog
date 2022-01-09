44e5c18b5fa7bd17127c96803f92deefb16186d9
GannouniMariem <mariem.gannouni@esen.tn>
2022-01-09 23:15:32 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <h1>Boo Blog!</h1>
+            <nav className="links">
+                <li><a href="/">Home</a></li>
+                <li><a href="/create">New Boo</a></li>
+            </nav>
+        </div>
+     );
+}
+ 
+export default Navbar;
